a0f77a7bbd51ff48e34b9d5328615363e68340c2
Victor Romero <romeroa.victor@yahoo.com>
2020-08-11 20:43:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,8 @@ def introduction(name)
     #{language}."
     end
     def introduction_with_language_optional(name, language = "Ruby")
-      puts 
-      
+      puts "Hi my name is #{name} and I am learning to program in #{language}."
+    end
       
       
       
